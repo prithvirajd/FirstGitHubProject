87d552bffbe5981078864f8b066373f7435106b3
tempuser <tempuser@ITSPUNE60.itspune.local>
2017-07-25 14:53:42 +0530

    Added for loop.

--- FirstGitHubProject/src/pd/Data.java
+++ FirstGitHubProject/src/pd/Data.java
@@ -2,8 +2,12 @@ package pd;
 
 public class Data {
 	public void display(){
-		System.out.println("Welcome to the world of GitHub!!!");
-		System.out.println("Enjoy GitHub.");
-		System.out.println("Let's Celebrate!");
+		
+		String[] names = {"Kunal", "Rajendra", "Tanaji", "Prithviraj"};
+		for(String name : names){
+			System.out.println("Welcome '"+name+"' to the world of GitHub!!!");
+			System.out.println("Enjoy GitHub.");
+			System.out.println("-----------------------------------");
+		}
 	}
 }
