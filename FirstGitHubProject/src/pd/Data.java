6655bd1609e427779b8605dff4f66602867a70f7
prithviraj <prithviraj.deshmukh@itshastra.com>
2017-07-25 11:56:46 +0530

    Initial Checkin of FirstGitHubProject

---
+++ FirstGitHubProject/src/pd/Data.java
@@ -0,0 +1,7 @@
+package pd;
+
+public class Data {
+	public void display(){
+		System.out.println("In the data file.");
+	}
+}
---
+++ FirstGitHubProject/src/pd/SampleDemo.java
@@ -0,0 +1,12 @@
+package pd;
+
+public class SampleDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello ! This is just for the GitHub Demo purpose.");
+		
+		Data dt = new Data();
+		dt.display();
+	}
+
+}
